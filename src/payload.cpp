4aa0347b78ea9742924efd9afea51b59129becbc
Thelolguy1 <lunar.lattice0@gmail.com>
2025-07-13 23:55:47 -0700

    Non-functional http server with crowcpp

--- src/payload.cpp
+++ src/payload.cpp
@@ -1,23 +1,42 @@
 #include "payload.hpp"
+#include "crow/logging.h"
 #include <consoleapi.h>
 #include <windows.h>
 #include <winnt.h>
 #include <winuser.h>
 
+#ifndef BINDADDR
+#define BINDADDR "0.0.0.0"
+#endif
+
+#ifndef BINDPORT
+#define BINDPORT 8080
+#endif
+
+void Run() {
+    crow::SimpleApp app;
+
+    CROW_ROUTE(app, "/api")([](){
+        return "Hello wrold";
+    });
+
+    app
+        .port(BINDPORT)
+        .loglevel(crow::LogLevel::Debug)
+        .multithreaded()
+        .run();
+}
+
 bool __stdcall DllMain(void *, std::uint32_t reason, void *) {
     if (reason == DLL_PROCESS_ATTACH) {
         AllocConsole();
-    }
-    return true;
-}
+        freopen("CONOUT$","w", stdout);
+        freopen("CONOUT$","w", stderr);
 
-/*
-BOOL WINAPI DllMain(HANDLE hDll, DWORD dwReason, LPVOID LpReserved) {
-    switch (dwReason) {
-        case DLL_PROCESS_ATTACH:
-            MessageBox(NULL, "TEST", "TEST", MB_OK);
-        break;
+        std::cout << "Trying to start HTTP Server..." << std::endl;
+        Run();
+        return true;
+    } else {
+    return false;
     }
-    return TRUE;
 }
-*/
--- src/payload.hpp
+++ src/payload.hpp
@@ -1,8 +1,9 @@
 #pragma once
+
 #include <cstdint>
 #define DLL_EXPORT extern "C" __declspec(dllexport)
-#include <cstddef>
-#include <windows.h>
+#include <crow.h>
 
-//BOOL WINAPI DllMain(HANDLE hDll, DWORD dwReason, LPVOID LpReserved);
 bool __stdcall DllMain(void *, std::uint32_t reason, void *);
+
+void Run();
